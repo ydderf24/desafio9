7538b2e3005adeecc82044fc9a40ecf62e4938a7
Freddy <freddy.moreno.berrios@gmail.com>
2021-08-11 21:55:49 -0300

    para enviar al profesor

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 const express = require('express'); 
+const path = require('path');
+const handlebars = require('express-handlebars');
+const rutas = require('./routes/rutas.js');
 const app = express();
 const port = 8080;
-const rutas = require('./routes/rutas.js');
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+const publicFolderPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicFolderPath));
+const layoutFolderPath = path.resolve(__dirname, '../views/layouts');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layoutsDir: layoutFolderPath,
+}));
+
+
+app.get('/', (req, res) =>{
+
+  res.render('main', {layout:'index.hbs'})
+});
+
+
+
+
+
+
+
 // const borrar = require('./routes/borrar');
 // const actualizar = require('./routes/actualizar');
-// 
 
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
-app.use('/api/rutas', rutas);
-// app.use('/api/borrar', borrar);
-// app.use('/api/actualizar', actualizar);
-
-
-// app.get('/api/productos/listar/:id' , (req, res) => {
-
-//   const idBuscado = req.params.id;
-
-//   const producto = productos.find((aProduct) => aProduct.id == idBuscado);
-
-//   if (!producto) {
-//     return res.status(404).json({
-//       msg: 'No existe un producto con este id',
-//     });
-//   }
-
-//   res.json({
-//     data: producto,
-//   });
-//   })
+// app.use('/api/rutas', rutas);
