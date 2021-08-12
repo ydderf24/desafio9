4c00019e90cdc15c31b2cd5eb1b2b7c149325763
Freddy <freddy.moreno.berrios@gmail.com>
2021-08-11 22:15:22 -0300

    ahora si

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express'); 
 const path = require('path');
-const handlebars = require('express-handlebars');
 const rutas = require('./routes/rutas.js');
 const app = express();
 const port = 8080;
@@ -8,20 +7,6 @@ const port = 8080;
 const server = app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-const publicFolderPath = path.resolve(__dirname, '../public');
-app.use(express.static(publicFolderPath));
-const layoutFolderPath = path.resolve(__dirname, '../views/layouts');
-app.set('view engine', 'hbs');
-app.engine('hbs', handlebars({
-  layoutsDir: layoutFolderPath,
-}));
-
-
-app.get('/', (req, res) =>{
-
-  res.render('main', {layout:'index.hbs'})
-});
-
 
 
 
@@ -34,4 +19,4 @@ app.get('/', (req, res) =>{
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/rutas', rutas);
+app.use('/api/rutas', rutas);
